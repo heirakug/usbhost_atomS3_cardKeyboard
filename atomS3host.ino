7995e3aaa3e64dba35cdb54cdcecf705303df8fb
heirakug <heirakug@yahoo.co.jp>
2024-05-17 22:40:25 +0900

    Update atomS3host.ino
    USBホスト

--- atomS3host.ino
+++ atomS3host.ino
@@ -1,5 +1,11 @@
 #include <M5Unified.h>
 #include <Wire.h>
+#include "USB.h"
+#include "USBHIDKeyboard.h"
+USBHIDKeyboard Keyboard;
+
+const int buttonPin = 41;
+int previousButtonState = HIGH;
 
 #define CARDKB_ADDR 0x5F
 #define SDA_PIN 2
@@ -7,22 +13,30 @@
 
 void setup() {
   auto cfg = M5.config();
-  cfg.external_spk = false;
+  cfg.external_spk = false;  // 外部スピーカーを無効化
   M5.begin(cfg);
-  
-  Wire.begin(SDA_PIN, SCL_PIN);
+
+  Wire.begin(SDA_PIN, SCL_PIN);  // SDA, SCL ピンの指定（M5AtomS3 LiteのG1とG2ピン）
   Serial.begin(115200);
+
+  pinMode(buttonPin, INPUT_PULLUP);
+  Keyboard.begin();
+  USB.begin();
 }
 
 void loop() {
-  Wire.requestFrom(CARDKB_ADDR, 1);
-  while (Wire.available()) {
-    char c = Wire.read();
-    if (c != 0) {
-      Serial.print(c);  // シリアルモニタにキーボードの入力を出力
-
-      // キーボード入力に応じて追加の処理を行うことができます
+  int buttonState = digitalRead(buttonPin);
+  if ((buttonState != previousButtonState) && (buttonState == LOW)) {
+    Wire.requestFrom(CARDKB_ADDR, 1);
+    while (Wire.available()) {
+      char c = Wire.read();
+      if (c != 0) {
+        Serial.print(c);  // シリアルモニタにキーボードの入力を出力
+        Keyboard.print(c);
+        // キーボード入力に応じて追加の処理を行うことができます
+      }
     }
   }
+  previousButtonState = buttonState;
   delay(10);
 }
