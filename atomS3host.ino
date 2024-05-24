409ba883eacf79ed5d66f6dbf808c055b9423021
heirakug <heirakug@yahoo.co.jp>
2024-05-24 14:13:19 +0900

    Update atomS3host.ino
    ESCキーの追加、stampS3に対応

--- atomS3host.ino
+++ atomS3host.ino
@@ -1,3 +1,5 @@
+#define 
+
 #include <M5Unified.h>
 #include <Wire.h>
 #include "USB.h"
@@ -5,15 +7,15 @@
 USBHIDKeyboard Keyboard;
 
 #define CARDKB_ADDR 0x5F
-#define SDA_PIN 2
-#define SCL_PIN 1
+#define SDA_PIN 13 //stomS3:2,stampS3:13
+#define SCL_PIN 15 //stomS3:1,stampS3:15
 
 void setup() {
   auto cfg = M5.config();
   cfg.external_spk = false;  // 外部スピーカーを無効化
   M5.begin(cfg);
 
-  Wire.begin(SDA_PIN, SCL_PIN);  // SDA, SCL ピンの指定（M5AtomS3 LiteのG1とG2ピン）
+  Wire.begin(SDA_PIN, SCL_PIN);  // SDA, SCL ピンの指定（M5AtomS3 LiteのG1とG2ピン / m5stampS3の13と15）
   Serial.begin(115200);
 
   Keyboard.begin();
@@ -47,6 +49,9 @@ void loop() {
         Keyboard.println();
       } else if (c == '\n') {
         // LF（改行）を受信したら何もしない（CRとのペアで処理される）
+      } else if (c == 0x1B) {
+        // ESCキーを受信した場合
+        Keyboard.write(KEY_ESC);  // ESCキーを送信
       } else {
         Serial.print(c);  // シリアルモニタにキーボードの入力を出力
         Keyboard.print(c);
