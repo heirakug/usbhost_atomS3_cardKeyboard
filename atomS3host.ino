44e11e40ae82c92a3670fc55784985fae5b08a82
heirakug <heirakug@yahoo.co.jp>
2024-05-24 11:50:00 +0900

    Update atomS3host.ino
    改行コード追加

--- atomS3host.ino
+++ atomS3host.ino
@@ -21,14 +21,38 @@ void setup() {
 }
 
 void loop() {
-    Wire.requestFrom(CARDKB_ADDR, 1);
-    while (Wire.available()) {
-      char c = Wire.read();
-      if (c != 0) {
+  Wire.requestFrom(CARDKB_ADDR, 1);
+  while (Wire.available()) {
+    char c = Wire.read();
+    /*
+    // debug
+    if (c != 0) {
+      // 受信したデータを16進数形式で出力
+      Serial.print("Received data in hex: 0x");
+      Keyboard.print("Received data in hex: 0x");
+      if (c < 0x10) {
+        Serial.print("0");  // 1桁の場合は0を追加
+        Keyboard.print("0");
+      }
+      Serial.println(c, HEX);
+      Keyboard.println(c, HEX);
+    }
+    */
+
+    if (c != 0) {
+
+      if (c == '\r') {
+        // CR（キャリッジリターン）を受信したら改行を出力
+        Serial.println();
+        Keyboard.println();
+      } else if (c == '\n') {
+        // LF（改行）を受信したら何もしない（CRとのペアで処理される）
+      } else {
         Serial.print(c);  // シリアルモニタにキーボードの入力を出力
         Keyboard.print(c);
         // キーボード入力に応じて追加の処理を行うことができます
       }
     }
+  }
   delay(10);
 }
